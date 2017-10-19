8a77c022ec5cca57a50adcf3092074db488f8fd9
Kyle Conley <kyle.conley@me.com>
2017-10-19 11:10:24 -0700

    moved favicon

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var path = require('path');
 
 app = express();
 
+app.get('/favicon.ico', function(req, res) {
+    res.status(204);
+});
 app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || 5000;
 
 app.listen(port);
 
-app.get('/favicon.ico', function(req, res) {
-    res.status(204);
-});
 
 console.log('server started ' + port);
